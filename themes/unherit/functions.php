094504f1f4c9f1e4fd1f20b377594c2356d652c9
Jake Carlson <jake@carlsonville.com>
2018-04-22 00:48:25 -0500

    Added custom pagination mechanism.

--- themes/unherit/functions.php
+++ themes/unherit/functions.php
@@ -95,6 +95,36 @@ function unherit_get_pagenum_link($num) {
     return $url;
 }
 
+function unherit_get_previous_posts_link( $label = null ) {
+    global $paged;
+
+    if ( null === $label )
+        $label = __( '&laquo; Previous Page' );
+
+    if ( !is_single() && $paged > 1 ) {
+        /**
+         * Filters the anchor tag attributes for the previous posts page link.
+         *
+         * @since 2.7.0
+         *
+         * @param string $attributes Attributes for the anchor tag.
+         */
+        $attr = apply_filters( 'previous_posts_link_attributes', '' );
+        return '<a href="' . unherit_get_previous_posts_page_link( false ) . "\" $attr>". preg_replace( '/&([^#])(?![a-z]{1,8};)/i', '&#038;$1', $label ) .'</a>';
+    }
+}
+
+function unherit_get_previous_posts_page_link() {
+    global $paged;
+
+    if ( !is_single() ) {
+        $nextpage = intval($paged) - 1;
+        if ( $nextpage < 1 )
+            $nextpage = 1;
+        return unherit_get_pagenum_link($nextpage);
+    }
+}
+
 function unherit_get_next_posts_link( $label = null, $max_page = 0 ) {
     global $paged, $wp_query;
 
